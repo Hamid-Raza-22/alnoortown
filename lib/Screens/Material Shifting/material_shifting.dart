d7691225e7127b788acffca489981f362703886e
Arfa <arfahere@gmail.com>
2024-07-09 12:27:49 +0500

    added two more screens - 9 july 2024

--- lib/Screens/Material Shifting/material_shifting.dart
+++ lib/Screens/Material Shifting/material_shifting.dart
@@ -1,36 +1,234 @@
 import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
 
 class MaterialShiftingPage extends StatefulWidget {
   const MaterialShiftingPage({super.key});
 
   @override
-  State<MaterialShiftingPage> createState() => _MaterialShiftingPageState();
+  _MaterialShiftingPageState createState() => _MaterialShiftingPageState();
 }
 
 class _MaterialShiftingPageState extends State<MaterialShiftingPage> {
+  final List<String> blocks = ["Block A", "Block B", "Block C", "Block D", "Block E", "Block F", "Block G"];
+  final List<String> streets = ["Block A", "Block B", "Block C", "Block D", "Block E", "Block F", "Block G"];
+  List<Map<String, dynamic>> containerDataList = [];
+
+  @override
+  void initState() {
+    super.initState();
+    containerDataList.add(createInitialContainerData());
+  }
+
+  Map<String, dynamic> createInitialContainerData() {
+    return {
+      "selectedBlock": null,
+      "selectedStreet": null,
+      "selectedShifting": 0,
+    };
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-        title: const Text('Material Shifting'),
-        backgroundColor: const Color(0xFF0D0700),
-      ),
-      body: Center(
-        child: Container(
-          padding: const EdgeInsets.all(20.0),
-          decoration: BoxDecoration(
-            color: Colors.amber,
-            borderRadius: BorderRadius.circular(10.0),
+      appBar: PreferredSize(
+        preferredSize: const Size.fromHeight(200.0),
+        child: AppBar(
+          backgroundColor: Colors.transparent,
+          elevation: 0,
+          flexibleSpace: Stack(
+            children: [
+              Container(
+                width: double.infinity,
+                height: double.infinity,
+                decoration: const BoxDecoration(
+                  image: DecorationImage(
+                    image: AssetImage('assets/images/trackter-01.png'),
+                    fit: BoxFit.fitHeight,
+                  ),
+                ),
+              ),
+            ],
           ),
-          child: const Text(
-            'Material',
-            style: TextStyle(
-              fontSize: 24.0,
-              fontWeight: FontWeight.bold,
-            ),
-          ),
+          systemOverlayStyle: SystemUiOverlayStyle.dark,
         ),
       ),
+      body: SingleChildScrollView(
+        padding: const EdgeInsets.all(16.0),
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: [
+            const Align(
+              alignment: Alignment.center,
+              child: Padding(
+                padding: EdgeInsets.only(bottom: 16.0),
+                child: Text(
+                  'Shifting Work',
+                  style: TextStyle(fontSize: 24, fontWeight: FontWeight.bold, color: Color(0xFFC69840)),
+                ),
+              ),
+            ),
+            ...containerDataList.asMap().entries.map((entry) {
+              int index = entry.key;
+              return Column(
+                children: [
+                  buildContainer(index),
+                  const SizedBox(height: 16),
+                ],
+              );
+            }),
+            const SizedBox(height: 16),
+            Center(
+              child: FloatingActionButton(
+                onPressed: () {
+                  setState(() {
+                    containerDataList.add(createInitialContainerData());
+                  });
+                },
+                backgroundColor: Colors.transparent,
+                elevation: 0, // No shadow
+                child: const Icon(Icons.add, color: Color(0xFFC69840), size: 36.0), // Increase size of the icon
+              ),
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+
+  Widget buildContainer(int index) {
+    var containerData = containerDataList[index];
+    bool isFilled = containerData["selectedBlock"] != null &&
+        containerData["selectedStreet"] != null &&
+        containerData["selectedShifting"] != null;
+
+    return Card(
+      margin: const EdgeInsets.only(bottom: 16),
+      elevation: 3,
+      shape: RoundedRectangleBorder(borderRadius: BorderRadius.circular(15)),
+      color: Colors.white,
+      child: Padding(
+        padding: const EdgeInsets.all(20.0),
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: [
+            buildBlockStreetRow(containerData),
+            const SizedBox(height: 16),
+            Row(
+              mainAxisAlignment: MainAxisAlignment.spaceBetween,
+              children: [
+                Text(
+                  "No. of Shifting",
+                  style: TextStyle(fontSize: 14, fontWeight: FontWeight.bold, color: Color(0xFFC69840)),
+                ),
+                Row(
+                  children: [
+                    IconButton(
+                      icon: const Icon(Icons.remove),
+                      onPressed: () {
+                        setState(() {
+                          if (containerData["selectedShifting"] > 0) {
+                            containerData["selectedShifting"]--;
+                          }
+                        });
+                      },
+                    ),
+                    Text(
+                      containerData["selectedShifting"].toString(),
+                      style: const TextStyle(fontSize: 16, fontWeight: FontWeight.bold),
+                    ),
+                    IconButton(
+                      icon: const Icon(Icons.add),
+                      onPressed: () {
+                        setState(() {
+                          containerData["selectedShifting"]++;
+                        });
+                      },
+                    ),
+                  ],
+                ),
+              ],
+            ),
+            const SizedBox(height: 20),
+            Center(
+              child: ElevatedButton(
+                onPressed: () {
+                  final selectedBlock = containerData["selectedBlock"];
+                  final selectedStreet = containerData["selectedStreet"];
+                  final selectedShifting = containerData["selectedShifting"];
+
+                  ScaffoldMessenger.of(context).showSnackBar(
+                    SnackBar(
+                      content: Text(
+                        'Selected: $selectedBlock, $selectedStreet, No. of Shifting: $selectedShifting',
+                      ),
+                    ),
+                  );
+                },
+                style: ElevatedButton.styleFrom(
+                  backgroundColor: const Color(0xFFF3F4F6),
+                  padding: const EdgeInsets.symmetric(horizontal: 20, vertical: 12),
+                  textStyle: const TextStyle(fontSize: 14),
+                  shape: const RoundedRectangleBorder(
+                    borderRadius: BorderRadius.zero,
+                  ),
+                ),
+                child: const Text('Submit', style: TextStyle(fontWeight: FontWeight.bold, color: Color(0xFFC69840))),
+              ),
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+
+  Widget buildBlockStreetRow(Map<String, dynamic> containerData) {
+    return Row(
+      children: [
+        Expanded(
+          child: buildDropdownField(
+              "From Block", containerData, "selectedBlock", blocks
+          ),
+        ),
+        const SizedBox(width: 16),
+        Expanded(
+          child: buildDropdownField(
+              "To Block", containerData, "selectedStreet", streets
+          ),
+        ),
+      ],
+    );
+  }
+
+  Widget buildDropdownField(String title, Map<String, dynamic> containerData, String key, List<String> items) {
+    return Column(
+      crossAxisAlignment: CrossAxisAlignment.start,
+      children: [
+        Text(
+          title,
+          style: const TextStyle(fontSize: 14, fontWeight: FontWeight.bold, color: Color(0xFFC69840)),
+        ),
+        const SizedBox(height: 8),
+        DropdownButtonFormField<String>(
+          value: containerData[key],
+          items: items.map((item) {
+            return DropdownMenuItem(
+              value: item,
+              child: Text(item),
+            );
+          }).toList(),
+          onChanged: (value) {
+            setState(() {
+              containerData[key] = value;
+            });
+          },
+          decoration: const InputDecoration(
+            border: OutlineInputBorder(
+              borderSide: BorderSide(color: Color(0xFFC69840)),
+            ),
+            contentPadding: EdgeInsets.symmetric(horizontal: 8),
+          ),
+        ),
+      ],
     );
   }
 }
--- lib/Screens/New Material/new_material.dart
+++ lib/Screens/New Material/new_material.dart
@@ -1,15 +1,282 @@
 import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
 
 class NewMaterial extends StatefulWidget {
-  const NewMaterial({super.key});
+  const NewMaterial({Key? key}) : super(key: key);
 
   @override
-  State<NewMaterial> createState() => _NewMaterialState();
+  _NewMaterialState createState() => _NewMaterialState();
 }
 
 class _NewMaterialState extends State<NewMaterial> {
+  List<Map<String, dynamic>> containerDataList = [];
+
+  @override
+  void initState() {
+    super.initState();
+    containerDataList.add(createInitialContainerData());
+  }
+
+  Map<String, dynamic> createInitialContainerData() {
+    return {
+      "sand": 0,
+      "soil": 0,
+      "base": 0,
+      "subBase": 0,
+      "waterBound": 0,
+      "otherMaterial": "",
+      "otherMaterialValue": 0,
+    };
+  }
+
   @override
   Widget build(BuildContext context) {
-    return const Scaffold();
+    return Scaffold(
+      appBar: PreferredSize(
+        preferredSize: const Size.fromHeight(180.0),
+        child: AppBar(
+          backgroundColor: Colors.transparent,
+          elevation: 0,
+          flexibleSpace: Stack(
+            children: [
+              Container(
+                width: double.infinity,
+                height: double.infinity,
+                decoration: const BoxDecoration(
+                  image: DecorationImage(
+                    image: AssetImage('assets/images/new_material-01.png'),
+                    fit: BoxFit.fitHeight,
+                  ),
+                ),
+              ),
+            ],
+          ),
+          systemOverlayStyle: SystemUiOverlayStyle.dark,
+        ),
+      ),
+      body: SingleChildScrollView(
+        padding: const EdgeInsets.all(16.0),
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: [
+            const SizedBox(height: 20),
+            Center(
+              child: Text(
+                'New Material',
+                style: TextStyle(fontSize: 24, fontWeight: FontWeight.bold, color: Color(0xFFC69840)),
+              ),
+            ),
+            ...containerDataList.asMap().entries.map((entry) {
+              int index = entry.key;
+              return Column(
+                children: [
+                  buildContainer(index),
+                  const SizedBox(height: 16),
+                ],
+              );
+            }),
+            const SizedBox(height: 16),
+            Center(
+              child: FloatingActionButton(
+                onPressed: () {
+                  setState(() {
+                    containerDataList.add(createInitialContainerData());
+                  });
+                },
+                backgroundColor: Colors.transparent,
+                elevation: 0,
+                child: const Icon(Icons.add, color: Color(0xFFC69840), size: 36.0),
+              ),
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+
+  Widget buildContainer(int index) {
+    var containerData = containerDataList[index];
+
+    return Card(
+      margin: const EdgeInsets.only(bottom: 16),
+      elevation: 3,
+      shape: RoundedRectangleBorder(borderRadius: BorderRadius.circular(15)),
+      color: Colors.white,
+      child: Padding(
+        padding: const EdgeInsets.all(20.0),
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: [
+            buildLabelsAndFields(index, containerData, ["Sand", "Soil"]),
+            const SizedBox(height: 16),
+            buildLabelsAndFields(index, containerData, ["Base", "Sub Base"]),
+            const SizedBox(height: 16),
+            buildLabelsAndFields(index, containerData, ["Water Bound"]),
+            const SizedBox(height: 16),
+            buildOtherMaterialField(index, containerData),
+            const SizedBox(height: 20),
+            Center(
+              child: ElevatedButton(
+                onPressed: () {
+                  ScaffoldMessenger.of(context).showSnackBar(
+                    SnackBar(
+                      content: Text(
+                        'Data Submitted for index $index: $containerData',
+                      ),
+                    ),
+                  );
+                },
+                style: ElevatedButton.styleFrom(
+                  backgroundColor: const Color(0xFFF3F4F6),
+                  padding: const EdgeInsets.symmetric(horizontal: 20, vertical: 12),
+                  textStyle: const TextStyle(fontSize: 14),
+                  shape: const RoundedRectangleBorder(
+                    borderRadius: BorderRadius.zero,
+                  ),
+                ),
+                child: const Text('Submit', style: TextStyle(fontWeight: FontWeight.bold, color: Color(0xFFC69840))),
+              ),
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+
+  Widget buildLabelsAndFields(int index, Map<String, dynamic> containerData, List<String> labels) {
+    return Row(
+      mainAxisAlignment: MainAxisAlignment.center,
+      children: labels.map((label) {
+        String fieldName = getFieldName(label);
+        int value = containerData[fieldName] ?? 0;
+        return Expanded(
+          child: buildStepperField(label, containerData, fieldName, value),
+        );
+      }).toList(),
+    );
+  }
+
+  Widget buildOtherMaterialField(int index, Map<String, dynamic> containerData) {
+    return Column(
+      crossAxisAlignment: CrossAxisAlignment.start,
+      children: [
+        const Text(
+          "Other Material",
+          style: TextStyle(fontSize: 14, fontWeight: FontWeight.bold, color: Color(0xFFC69840)),
+        ),
+        const SizedBox(height: 8),
+        Container(
+          decoration: BoxDecoration(
+            border: Border.all(color: Color(0xFFC69840)),
+            borderRadius: BorderRadius.circular(4.0),
+          ),
+          child: Row(
+            children: [
+              Expanded(
+                child: TextField(
+                  onChanged: (value) {
+                    setState(() {
+                      containerData["otherMaterial"] = value;
+                    });
+                  },
+                  decoration: InputDecoration(
+                    hintText: 'Other Material',
+                    hintStyle: TextStyle(color: Colors.grey.withOpacity(0.4)),
+                    border: InputBorder.none,
+                    contentPadding: EdgeInsets.symmetric(horizontal: 8.0),
+                  ),
+                  style: const TextStyle(color: Color(0xFFC69840)),
+                ),
+              ),
+              Row(
+                children: [
+                  IconButton(
+                    icon: Icon(Icons.remove, color: Color(0xFFC69840)),
+                    onPressed: () {
+                      setState(() {
+                        if (containerData["otherMaterialValue"] > 0) {
+                          containerData["otherMaterialValue"]--;
+                        }
+                      });
+                    },
+                  ),
+                  Text(
+                    '${containerData["otherMaterialValue"] ?? 0}',
+                    style: const TextStyle(fontSize: 16, color: Color(0xFFC69840)),
+                  ),
+                  IconButton(
+                    icon: Icon(Icons.add, color: Color(0xFFC69840)),
+                    onPressed: () {
+                      setState(() {
+                        containerData["otherMaterialValue"]++;
+                      });
+                    },
+                  ),
+                ],
+              ),
+            ],
+          ),
+        ),
+      ],
+    );
+  }
+
+  String getFieldName(String label) {
+    switch (label) {
+      case "Sub Base":
+        return "subBase";
+      case "Water Bound":
+        return "waterBound";
+      case "Other Material":
+        return "otherMaterial";
+      default:
+        return label.toLowerCase();
+    }
+  }
+
+  Widget buildStepperField(String label, Map<String, dynamic> containerData, String fieldName, int value) {
+    return Column(
+      crossAxisAlignment: CrossAxisAlignment.start,
+      children: [
+        Text(
+          label,
+          style: const TextStyle(fontSize: 14, fontWeight: FontWeight.bold, color: Color(0xFFC69840)),
+        ),
+        const SizedBox(height: 8),
+        Container(
+          decoration: BoxDecoration(
+            border: Border.all(color: Color(0xFFC69840)),
+            borderRadius: BorderRadius.circular(4.0),
+          ),
+          child: Row(
+            mainAxisSize: MainAxisSize.min,
+            children: [
+              IconButton(
+                icon: Icon(Icons.remove, color: Color(0xFFC69840)),
+                onPressed: () {
+                  setState(() {
+                    if (containerData[fieldName] > 0) {
+                      containerData[fieldName]--;
+                    }
+                  });
+                },
+              ),
+              Text(
+                '$value',
+                style: const TextStyle(fontSize: 16, color: Color(0xFFC69840)),
+              ),
+              IconButton(
+                icon: Icon(Icons.add, color: Color(0xFFC69840)),
+                onPressed: () {
+                  setState(() {
+                    containerData[fieldName]++;
+                  });
+                },
+              ),
+            ],
+          ),
+        ),
+      ],
+    );
   }
 }
