226157940adfd5e39fcf1fced2303513afe222fe
Stud-droid <sulemangujjer12345@gmail.com>
2024-09-02 11:04:40 +0500

    Suleman 11:04am 2 sept

--- lib/Screens/Building Work/Mosque/MosqueExcavationWork/mosque_summary_page.dart
+++ lib/Screens/Building Work/Mosque/MosqueExcavationWork/mosque_summary_page.dart
@@ -26,7 +26,7 @@ class _MosqueSummaryPageState extends State<MosqueSummaryPage> {
           },
         ),
         title: const Text(
-          'Summary',
+          'Summaryo',
           style: TextStyle(
             fontSize: 18,
             fontWeight: FontWeight.bold,
