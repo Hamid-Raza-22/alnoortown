2be0f98797d1b38487ef27b0501b7576f57c275c
Arfa <arfahere@gmail.com>
2024-09-28 15:32:38 +0500

    base and sub base

--- lib/Screens/BuildingWork/RoadsCompactionWork/BaseSubBaseCompaction/BaseSubbaseSummary.dart
+++ lib/Screens/BuildingWork/RoadsCompactionWork/BaseSubBaseCompaction/BaseSubbaseSummary.dart
@@ -4,16 +4,43 @@ import 'package:flutter/material.dart';
 import 'package:get/get_core/src/get_main.dart';
 import 'package:get/get_instance/get_instance.dart';
 import 'package:intl/intl.dart';
-import 'package:get/get.dart' show  Get, Inst, Obx;
+import 'package:get/get.dart' show Get, Inst, Obx;
+import '../../../ReusableDesigns/filter_widget.dart';
+
 
 class BaseSubBaseCompactionSummary extends StatefulWidget {
-    BaseSubBaseCompactionSummary({super.key});
+  BaseSubBaseCompactionSummary({super.key});
   @override
-  State<BaseSubBaseCompactionSummary> createState() => _BaseSubBaseCompactionSummaryState();
+  State<BaseSubBaseCompactionSummary> createState() =>
+      _BaseSubBaseCompactionSummaryState();
 }
 
-class _BaseSubBaseCompactionSummaryState extends State<BaseSubBaseCompactionSummary> {
-  final BaseSubBaseCompactionViewModel baseSubBaseCompactionViewModel = Get.put(BaseSubBaseCompactionViewModel());
+class _BaseSubBaseCompactionSummaryState
+    extends State<BaseSubBaseCompactionSummary> {
+  final BaseSubBaseCompactionViewModel baseSubBaseCompactionViewModel =
+  Get.put(BaseSubBaseCompactionViewModel());
+
+  // Filtered list to store filtered data
+  List filteredList = [];
+
+  @override
+  void initState() {
+    super.initState();
+    // Initially, show all data
+    filteredList = baseSubBaseCompactionViewModel.allSubBase;
+  }
+
+  void _applyFilter(DateTime? fromDate, DateTime? toDate, String? block) {
+    setState(() {
+      filteredList = baseSubBaseCompactionViewModel.allSubBase.where((entry) {
+        bool matchesBlock = block == null || block.isEmpty || (entry.block_no != null && entry.block_no!.contains(block));
+        bool matchesFromDate = fromDate == null || (entry.start_date != null && entry.start_date!.isAfter(fromDate.subtract(Duration(days: 1))));
+        bool matchesToDate = toDate == null || (entry.expected_comp_date != null && entry.expected_comp_date!.isBefore(toDate.add(Duration(days: 1))));
+
+        return matchesBlock && matchesFromDate && matchesToDate;
+      }).toList();
+    });
+  }
 
   @override
   Widget build(BuildContext context) {
@@ -22,12 +49,12 @@ class _BaseSubBaseCompactionSummaryState extends State<BaseSubBaseCompactionSumm
       appBar: AppBar(
         backgroundColor: Colors.white,
         leading: IconButton(
-          icon:   Icon(Icons.arrow_back, color: Color(0xFFC69840)),
+          icon: Icon(Icons.arrow_back, color: Color(0xFFC69840)),
           onPressed: () {
             Navigator.pop(context);
           },
         ),
-        title:   Text(
+        title: Text(
           'base_sub_base_compaction_summary'.tr(),
           style: TextStyle(
               fontSize: 14, fontWeight: FontWeight.bold, color: Color(0xFFC69840)),
@@ -35,76 +62,103 @@ class _BaseSubBaseCompactionSummaryState extends State<BaseSubBaseCompactionSumm
         centerTitle: true,
       ),
       body: Padding(
-        padding:   EdgeInsets.all(12.0),
-          child: Obx(() {
-            // Use Obx to rebuild when the data changes
-            if (baseSubBaseCompactionViewModel.allSubBase.isEmpty) {
-              return Center(
-                  child: CircularProgressIndicator()); // Show loading indicator
-            }
+        padding: EdgeInsets.all(12.0),
+        child: Column(
+          children: [
+            // Call the FilterWidget here
+            FilterWidget(
+              onFilter: (DateTime? fromDate, DateTime? toDate, String? block) {
+                _applyFilter(fromDate, toDate, block);
+              },
+            ),
+            Expanded(
+              child: Obx(() {
+                if (filteredList.isEmpty) {
+                  return Center(
+                    child: Column(
+                      mainAxisAlignment: MainAxisAlignment.center,
+                      children: [
+                        Image.asset(
+                          'assets/images/nodata.png',
+                          width: 200,
+                          height: 200,
+                          fit: BoxFit.cover,
+                        ),
+                        SizedBox(height: 16),
+                        Text(
+                          'No data available',
+                          style: TextStyle(
+                              color: Colors.grey,
+                              fontSize: 16,
+                              fontWeight: FontWeight.bold),
+                        ),
+                      ],
+                    ),
+                  );
+                }
 
-            return SingleChildScrollView(
-              scrollDirection: Axis.horizontal,
-              child: DataTable(
-                columnSpacing: 12.0,
-                headingRowColor: MaterialStateProperty.all(Color(0xFFC69840)),
-                border: TableBorder(
-                  horizontalInside: BorderSide(
-                      color: Color(0xFFC69840), width: 1.0),
-                  verticalInside: BorderSide(
-                      color: Color(0xFFC69840), width: 1.0),
-                ),
-                columns: [
-                  DataColumn(label: Text('block_no'.tr(),
-                      style: TextStyle(fontWeight: FontWeight.bold))),
-                  DataColumn(label: Text('road_no'.tr(),
-                      style: TextStyle(fontWeight: FontWeight.bold))),
-                  DataColumn(label: Text('total_length'.tr(),
-                      style: TextStyle(fontWeight: FontWeight.bold))),
-                  DataColumn(label: Text('start_date'.tr(),
-                      style: TextStyle(fontWeight: FontWeight.bold))),
-                  DataColumn(label: Text('end_date'.tr(),
-                      style: TextStyle(fontWeight: FontWeight.bold))),
-                  DataColumn(label: Text('status'.tr(),
-                      style: TextStyle(fontWeight: FontWeight.bold))),
-                  DataColumn(label: Text('date'.tr(),
-                      style: TextStyle(fontWeight: FontWeight.bold))),
-                  DataColumn(label: Text('time'.tr(),
-                      style: TextStyle(fontWeight: FontWeight.bold))),
-                ],
-                rows: baseSubBaseCompactionViewModel.allSubBase.map((entry) {
-                  // Format the DateTime objects to a readable string format
-                  String start_date = entry.start_date != null
-                      ? DateFormat('d MMM yyyy').format(entry.start_date!)
-                      : ''; // Show empty string if null
-
-                  String expected_comp_date = entry.expected_comp_date != null
-                      ? DateFormat('d MMM yyyy').format(entry.expected_comp_date!)
-                      : ''; // Show empty string if null
-                  return DataRow(cells: [
-                    // Formatted start date
-
-                    DataCell(Text(entry.block_no ?? '')),
-                    // Null check for status
-                    DataCell(Text(entry.road_no ?? '')),
-                    // Null check for status
-                    DataCell(Text(entry.total_length ?? '')),
-                    DataCell(Text(start_date)),
-                    DataCell(Text(expected_comp_date)),
-                    // Null check for status
-                    DataCell(Text(entry.base_sub_base_comp_status ?? '')),
-                    // Null check for status
-                    DataCell(Text(entry.date ?? '')),
-                    // Display date as-is (assuming it's already formatted)
-                    DataCell(Text(entry.time ?? '')),
-                    // Display time as-is (assuming it's already formatted)
-
-                  ]);
-                }).toList(),
-
-              ),
-            );
-          }),
+                return SingleChildScrollView(
+                  scrollDirection: Axis.horizontal,
+                  child: DataTable(
+                    columnSpacing: 12.0,
+                    headingRowColor:
+                    WidgetStateProperty.all(Color(0xFFC69840)),
+                    border: TableBorder(
+                      horizontalInside:
+                      BorderSide(color: Color(0xFFC69840), width: 1.0),
+                      verticalInside:
+                      BorderSide(color: Color(0xFFC69840), width: 1.0),
+                    ),
+                    columns: [
+                      DataColumn(
+                          label: Text('block_no'.tr(),
+                              style: TextStyle(fontWeight: FontWeight.bold))),
+                      DataColumn(
+                          label: Text('road_no'.tr(),
+                              style: TextStyle(fontWeight: FontWeight.bold))),
+                      DataColumn(
+                          label: Text('total_length'.tr(),
+                              style: TextStyle(fontWeight: FontWeight.bold))),
+                      DataColumn(
+                          label: Text('start_date'.tr(),
+                              style: TextStyle(fontWeight: FontWeight.bold))),
+                      DataColumn(
+                          label: Text('end_date'.tr(),
+                              style: TextStyle(fontWeight: FontWeight.bold))),
+                      DataColumn(
+                          label: Text('status'.tr(),
+                              style: TextStyle(fontWeight: FontWeight.bold))),
+                      DataColumn(
+                          label: Text('date'.tr(),
+                              style: TextStyle(fontWeight: FontWeight.bold))),
+                      DataColumn(
+                          label: Text('time'.tr(),
+                              style: TextStyle(fontWeight: FontWeight.bold))),
+                    ],
+                    rows: filteredList.map((entry) {
+                      String startDate = entry.start_date != null
+                          ? DateFormat('d MMM yyyy').format(entry.start_date!)
+                          : '';
+                      String expectedCompDate = entry.expected_comp_date != null
+                          ? DateFormat('d MMM yyyy').format(entry.expected_comp_date!)
+                          : '';
+                      return DataRow(cells: [
+                        DataCell(Text(entry.block_no ?? '')),
+                        DataCell(Text(entry.road_no ?? '')),
+                        DataCell(Text(entry.total_length ?? '')),
+                        DataCell(Text(startDate)),
+                        DataCell(Text(expectedCompDate)),
+                        DataCell(Text(entry.base_sub_base_comp_status ?? '')),
+                        DataCell(Text(entry.date ?? '')),
+                        DataCell(Text(entry.time ?? '')),
+                      ]);
+                    }).toList(),
+                  ),
+                );
+              }),
+            ),
+          ],
+        ),
       ),
     );
   }
